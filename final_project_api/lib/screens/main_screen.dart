2be3ed62299cee71529ac51c4d614b446c95e38b
Miguel <miguel.garon@gmail.com>
2024-01-04 13:15:39 +0100

    main screen buttons
    Added main screen buttons at the bottom to change to different screens (home, favorites,  meal plan, settings).

--- final_project_api/lib/screens/main_screen.dart
+++ final_project_api/lib/screens/main_screen.dart
@@ -14,6 +14,9 @@ class _MainScreenState extends State<MainScreen> {
   int currentTab = 0;
   List screens = const [
     HomeScreen(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
   ];
 
   @override
@@ -25,65 +28,85 @@ class _MainScreenState extends State<MainScreen> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              children: [
-                Icon(
-                  currentTab == 0 ? Iconsax.home5 : Iconsax.home,
-                  color: currentTab == 0 ? kprimaryColor : Colors.grey,
-                ),
-                Text(
-                  "Home",
-                  style: TextStyle(
-                    fontSize: 14,
+            GestureDetector(
+              onTap: () => setState(() {
+                currentTab = 0;
+              }),
+              child: Column(
+                children: [
+                  Icon(
+                    currentTab == 0 ? Iconsax.home5 : Iconsax.home,
                     color: currentTab == 0 ? kprimaryColor : Colors.grey,
                   ),
-                ),
-              ],
+                  Text(
+                    "Home",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: currentTab == 0 ? kprimaryColor : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Column(
-              children: [
-                Icon(
-                  currentTab == 1 ? Iconsax.heart5 : Iconsax.heart,
-                  color: currentTab == 1 ? kprimaryColor : Colors.grey,
-                ),
-                Text(
-                  "Favorites",
-                  style: TextStyle(
-                    fontSize: 14,
+            GestureDetector(
+              onTap: () => setState(() {
+                currentTab = 1;
+              }),
+              child: Column(
+                children: [
+                  Icon(
+                    currentTab == 1 ? Iconsax.heart5 : Iconsax.heart,
                     color: currentTab == 1 ? kprimaryColor : Colors.grey,
                   ),
-                ),
-              ],
+                  Text(
+                    "Favorites",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: currentTab == 1 ? kprimaryColor : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Column(
-              children: [
-                Icon(
-                  currentTab == 2 ? Iconsax.calendar_25 : Iconsax.calendar_2,
-                  color: currentTab == 2 ? kprimaryColor : Colors.grey,
-                ),
-                Text(
-                  "Meal Plan",
-                  style: TextStyle(
-                    fontSize: 14,
+            GestureDetector(
+              onTap: () => setState(() {
+                currentTab = 2;
+              }),
+              child: Column(
+                children: [
+                  Icon(
+                    currentTab == 2 ? Iconsax.calendar_25 : Iconsax.calendar_2,
                     color: currentTab == 2 ? kprimaryColor : Colors.grey,
                   ),
-                ),
-              ],
+                  Text(
+                    "Meal Plan",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: currentTab == 2 ? kprimaryColor : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Column(
-              children: [
-                Icon(
-                  currentTab == 3 ? Iconsax.setting5 : Iconsax.setting,
-                  color: currentTab == 3 ? kprimaryColor : Colors.grey,
-                ),
-                Text(
-                  "Settings",
-                  style: TextStyle(
-                    fontSize: 14,
+            GestureDetector(
+              onTap: () => setState(() {
+                currentTab = 3;
+              }),
+              child: Column(
+                children: [
+                  Icon(
+                    currentTab == 3 ? Iconsax.setting5 : Iconsax.setting,
                     color: currentTab == 3 ? kprimaryColor : Colors.grey,
                   ),
-                ),
-              ],
+                  Text(
+                    "Settings",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: currentTab == 3 ? kprimaryColor : Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
